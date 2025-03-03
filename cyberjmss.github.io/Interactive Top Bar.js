1a803fcf420005d4cde0732ef59a50fe07e6acf4
cyberjmss <mor0020@jmss.vic.edu.au>
2025-03-04 10:38:48 +1100

    Add files via upload

--- cyberjmss.github.io/Interactive Top Bar.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu")
-const menuItems = document.querySelector(".showMenue")
-const closeIcon = document.querySelector(".closeIcon")
-const hamBurger = document.querySelector(".hamBurger")
-
-const clickHandel = window.addEventListener("cl")
